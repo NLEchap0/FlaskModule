8195585951d19564065932449f5a6a9088bdd16f
NLEchap0 <phonestorage666999@gmail.com>
2026-09-01 15:54:35 +0200

    DEMO: templates

--- DEMO/main.py
+++ DEMO/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
@@ -7,13 +7,18 @@ app = Flask(__name__)
 def home():
     return "Welcome to the home page!"
 
-@app.route('/<text>')
+@app.route('/check/<text>')
 def text(text):
     try:
         text = float(text)
         return "It's a number"
     except:
         return "It's not a number"
+
+@app.route('/hello/')
+@app.route('/hello/<string:inputName>')
+def temmplate(inputName = "Are you dumb?"):
+    return render_template('hello.html', name=inputName)
 
 if __name__ == '__main__':
     app.run(debug=True)
