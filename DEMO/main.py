81b4192bc9cd371331781a6f7c51e63ecd3e165d
NLEchap0 <phonestorage666999@gmail.com>
2026-09-01 15:21:48 +0200

    DEMO: type checking

--- DEMO/main.py
+++ DEMO/main.py
@@ -2,13 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return "Welcome to the home page!"
 
-@app.route('/about')
-def about():
-    return "This is the about page."
+@app.route('/<text>')
+def text(text):
+    try:
+        text = float(text)
+        return "It's a number"
+    except:
+        return "It's not a number"
 
 if __name__ == '__main__':
     app.run(debug=True)
